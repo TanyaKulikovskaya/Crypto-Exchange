3495240163521bbb8fe182e0176acf99fdfaa657
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2023-06-12 10:25:16 +0300

    Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="h-full container mx-auto flex flex-col px-4 py-12 text-gray">
-    <h1 class="text-5xl leading-[3.75rem] mb-4">Crypto Exchange</h1>
+  <main class="h-full container mx-auto flex flex-col px-4 py-2 text-gray">
+    <h1 class="text-5xl leading-[3.75rem] mb-4 md:mt-10">Crypto Exchange</h1>
     <h3 class="text-xl leading-5 mb-[60px]">Exchange fast and easy</h3>
     <CryptoConverter />
   </main>
--- src/components/SearchableDropdown.vue
+++ src/components/SearchableDropdown.vue
@@ -8,7 +8,7 @@
       class="relative flex items-center justify-between h-full px-1 py-1 text-custom-gray border border-[#e3ebef] bg-[#f6f7f8] rounded-r-md hover:cursor-pointer"
       @click="toggleSelect"
     >
-      <div class="flex pr-2 pl-1">
+      <div class="flex pr-2 pl-1 w-4/5">
         <img :src="option.image" :alt="option.name" class="w-5 h-5 mr-3" />
         <span class="uppercase truncate mr-1">{{ option.ticker }}</span>
       </div>
